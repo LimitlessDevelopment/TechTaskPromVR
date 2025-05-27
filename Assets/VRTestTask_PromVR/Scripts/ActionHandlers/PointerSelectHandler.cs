f431b89fbb7ce5de57d3a8c240c0e48c5ba2d866
LimitlessDevelopment <47401152+LimitlessDevelopment@users.noreply.github.com>
2025-05-27 11:37:27 +0500

    +PointerSelectHandler

---
+++ Assets/VRTestTask_PromVR/Scripts/ActionHandlers/PointerSelectHandler.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(XRSimpleInteractable))]    
+public class PointerSelectHandler : ActionHandler
+{
+    private XRSimpleInteractable _interactable;
+
+    void Start()
+    {
+        // Подписываемся на событие "выбора" объекта лучом
+        _interactable.selectEntered.AddListener(OnPointerSelect);
+    }
+
+    private void OnPointerSelect(SelectEnterEventArgs args)
+    {
+        if (!isActiveAction)
+        {
+            // Кликнули на объект раньше, чем нужно
+            CompleteAction(withError: true);
+            return;
+        }
+        CompleteAction(withError: false);
+    }
+}
